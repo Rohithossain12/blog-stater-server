75bc3c09e2464cd588f4215defc32d298b4e876e
Rohithossain12 <rafidislamrohit576@gmail.com>
2025-09-16 06:01:49 +0600

    prisma initialize

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,27 @@
 import http, { Server } from "http";
 import dotenv from "dotenv";
 import app from "./app";
+import { prisma } from "./config/db";
 
 
 dotenv.config();
 
 let server: Server | null = null;
 
+
+async function connectToDB() {
+  try {
+    await prisma.$connect();
+    console.log("Database Connection Successfully");
+  } catch (error) {
+    console.log("Connection Failed", error);
+    process.exit(1);
+  }
+}
+
 async function startServer() {
   try {
+   await connectToDB();
     server = http.createServer(app);
     server.listen(process.env.PORT, () => {
       console.log(`🚀 Server is running on port ${process.env.PORT}`);
